696245e8ec86f7635c620228c0321a11f9a8628c
Jakis <ariovistus@gmail.com>
2013-07-06 21:25:46 -0600

    Updated web.js

--- web.js
+++ web.js
@@ -4,18 +4,29 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var indexdata;
 var content;
 
+function readContent(callback) {
+    fs.readFile("./Index.html", function (err, content) {
+        if (err) return callback(err)
+        callback(content)
+    })
+}
+
+readContent(function (content) {
+    console.log(content)
+})
+
+/*
 fs.readFile('./index.html', function (err, indexdata) {
   if (err) throw err;
   content = indexdata;
 });
-
-app.get('/', function(request, response) {
+*/
+//app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-callback(content);
-});
+//callback(content);
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
