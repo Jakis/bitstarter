bc3f47794d31e426fae32a3c6b1dd34e988d568b
Jakis <ariovistus@gmail.com>
2013-07-06 23:51:12 -0600

    Updated web.js

--- web.js
+++ web.js
@@ -1,18 +1,22 @@
-var http = require('http'),
-    fs = require('fs');
+fs = require('fs'); 
 var express = require('express');
 var app = express.createServer(express.logger());
+var indexdata;
 var content;
-var port = 5000;
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();
-        console.log("Listening on " + port);  
-    }).listen(5000);
+fs.readFile('./index.html', 'utf-8', function (err, indexdata) {
+  if (err) throw err;
+  content = indexdata;
+  console.log(content);
+});
+
+app.get('/', function(request, response) {
+//  response.send('Hello World2!');
+console.log(content);
+response.send(content);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
